17e08585f9bdfde944ecea1d6c40576203f2f476
EZTest AI <eztest.ai@commit.com>
2025-03-28 12:27:11 +0530

    Created new file: jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\.|/)(test|spec))\.ts$',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
